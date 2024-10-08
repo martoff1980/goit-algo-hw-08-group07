8bc2192b2e106f98fa9956ad87f6cfae82a8a800
martoff <martoff@ukr.net>
2024-10-08 05:10:21 +0300

    Update home_work8_main.py

--- home_work8_main.py
+++ home_work8_main.py
@@ -11,13 +11,28 @@ def heap_sort(iterable):
     return [heapq.heappop(h) for _ in range(len(h))]
 
 
+def min_coast(cables, n=0, cost=0):
+    global totalcost
+    if cables == []:
+        return
+    if n <= 1:
+        cost += cables.pop(0)
+        totalcost = cost
+    else:
+        cost += cables.pop(0)
+        totalcost += cost
+    min_coast(cables, n+1, cost)
+
+
 # набір кабелів, їх довжини, будуть як масив у вигляді цілих чисел
-cabels = [3, 2, 1, 4]
-print("Набір кабелів:", cabels)
+cables = [3, 2, 1, 4]
+print("Набір кабелів:", cables)
 
 # сортування кабелів
-sorted_cabels = heap_sort(cabels)
-print("Відсортованні кабеля:", sorted_cabels)
+sorted_cables = heap_sort(cables)
+print("Відсортованні кабеля:", sorted_cables)
 
-# всього
-print("Мінімальна вартість з'єднання:", sum(sorted_cabels))
+# очислення вартості за допомогою рекурсії
+totalcost = 0
+min_coast(sorted_cables)
+print("Мінімальна вартість з'єднання:", totalcost)
